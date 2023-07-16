04dae2d8c812f9cf2a5822b4b8cbea05af733970
kaynix <kaynix29@gmail.com>
2023-07-16 22:47:48 +0300

    fix grid height

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {  
   return (
-    <html lang="ru" className='h-full' data-theme="light">
+    <html lang="ru" className='h-full w-full' data-theme="light">
       {/* <body className='h-full mx-auto p-2 flex flex-1 flex-col'> */}
       <body className='md:container md:mx-auto px-4 py-8'>
         <Header/>
--- components/ContentBlock/PostItem/PostItem.tsx
+++ components/ContentBlock/PostItem/PostItem.tsx
@@ -1,13 +1,13 @@
 
 export default function PostItem({ id, title, content }) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-full h-full bg-base-100 shadow-xl">
   <figure><img src="/fr_jacket.webp" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">
     {title}
     </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <p>{content}</p>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">Fashion</div> 
       <div className="badge badge-outline">Products</div>
